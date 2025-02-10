1faa0375c5e0bf2b73c22798b02ec616e9a7b678
Sangmin Kim <offiksm0903@naver.com>
2025-02-10 22:09:57 +0900

    7-2: 내프로필 UI 및 기능 구현하기

--- LMessenger/Repository/UserDBRepository.swift
+++ LMessenger/Repository/UserDBRepository.swift
@@ -11,6 +11,8 @@ import FirebaseDatabase
 protocol UserDBRepositoryType {
     func addUser(_ object: UserObject) -> AnyPublisher<Void, DBError>
     func getUser(userId: String) -> AnyPublisher<UserObject, DBError>
+    func getUser(userId: String) async throws -> UserObject
+    func updateUser(userId: String, key: String, value: Any) async throws
     func loadUsers() -> AnyPublisher<[UserObject], DBError>
     func addUserAfterContact(users: [UserObject]) -> AnyPublisher<Void, DBError>
 }
@@ -66,6 +68,21 @@ class UserDBRepository: UserDBRepositoryType {
             }
         }
         .eraseToAnyPublisher()
+    }
+    
+    func getUser(userId: String) async throws -> UserObject {
+        // firebase realtimedatabse는 async를 지원함
+        guard let value = try await self.db.child(DBKey.Users).child(userId).getData().value else {
+            throw DBError.emptyValue
+        }
+        
+        let data = try JSONSerialization.data(withJSONObject: value)
+        let userObejct = try JSONDecoder().decode(UserObject.self, from: data)
+        return userObejct
+    }
+    
+    func updateUser(userId: String, key: String, value: Any) async throws {
+        try await self.db.child(DBKey.Users).child(userId).child(key).setValue(value)
     }
     
     func loadUsers() -> AnyPublisher<[UserObject], DBError> {
---
+++ LMessenger/View/Profile/MyProfileDescEditView.swift
@@ -0,0 +1,36 @@
+//
+//  MyProfileDescEditView.swift
+//  LMessenger
+//
+//
+
+import SwiftUI
+
+struct MyProfileDescEditView: View {
+    @Environment(\.dismiss) var dismiss
+    @State var description: String
+    
+    var onCompleted: (String) -> Void
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("상태메시지를 입력해주세요.", text: $description)
+                    .multilineTextAlignment(.center)
+            }
+            .toolbar {
+                Button("완료") {
+                    dismiss()
+                    onCompleted(description)
+                }
+                .disabled(description.isEmpty)
+            }
+        }
+    }
+}
+
+#Preview {
+    MyProfileDescEditView(description: "") { _ in
+        
+    }
+}
