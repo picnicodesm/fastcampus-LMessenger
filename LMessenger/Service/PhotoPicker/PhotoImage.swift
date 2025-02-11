665cb66c8c684878fd549c8f7dcadc5a360a8f0c
Sangmin Kim <offiksm0903@naver.com>
2025-02-11 12:34:29 +0900

    7-3: PhotosUI를 사용하여 PhotoPicker 구현하기

---
+++ LMessenger/Service/PhotoPicker/PhotoImage.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoImage.swift
+//  LMessenger
+//
+//
+
+import SwiftUI
+
+struct PhotoImage: Transferable {
+    
+    let data: Data
+    
+    static var transferRepresentation: some TransferRepresentation {
+        DataRepresentation(importedContentType: .image) { data in
+            // 원본에 대한 이미지 데이터라 용량이 크기 때문에 jpeg로 압축
+            guard let uiImage = UIImage(data: data) else {
+                throw PhotoPickerError.importFailed
+            }
+            
+            guard let data = uiImage.jpegData(compressionQuality: 0.3) else {
+                throw PhotoPickerError.importFailed
+            }
+            
+            return PhotoImage(data: data)
+        }
+    }
+}
--- LMessenger/Service/Services.swift
+++ LMessenger/Service/Services.swift
@@ -11,17 +11,20 @@ protocol ServiceType {
     var authService: AuthenticationServiceType { get set }
     var userService: UserServiceType { get set }
     var contactService: ContactServiceType { get set }
+    var photoPickerService: PhotoPickerServiceType { get set }
 }
 
 class Services: ServiceType {
     var authService: AuthenticationServiceType
     var userService: UserServiceType
     var contactService: ContactServiceType
+    var photoPickerService: any PhotoPickerServiceType
     
     init() {
         self.authService = AuthenticationService()
         self.userService = UserService(dbRepository: UserDBRepository())
         self.contactService = ContactService()
+        self.photoPickerService = PhotoPickerService()
     }
 }
 
@@ -29,4 +32,5 @@ class StubService: ServiceType {
     var authService: AuthenticationServiceType = StubAuthenticationService()
     var userService: UserServiceType = StubUserService()
     var contactService: ContactServiceType = StubContactService()
+    var photoPickerService: PhotoPickerServiceType = StubPhotoPickerService()
 }
