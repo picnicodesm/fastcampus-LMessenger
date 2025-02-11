4f13ccd01d30d86f97497532fca142c99cd36563
Sangmin Kim <offiksm0903@naver.com>
2025-02-11 12:34:29 +0900

    7-3: PhotosUI를 사용하여 PhotoPicker 구현하기

---
+++ LMessenger/Service/PhotoPicker/PhotoPickerService.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoPickerService.swift
+//  LMessenger
+//
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+enum PhotoPickerError: Error {
+    case importFailed
+}
+
+protocol PhotoPickerServiceType {
+    func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data
+}
+
+class PhotoPickerService: PhotoPickerServiceType {
+    func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data {
+        
+        guard let image = try await imageSelection.loadTransferable(type: PhotoImage.self) else {
+            throw PhotoPickerError.importFailed
+        }
+        return image.data
+    }
+}
+
+class StubPhotoPickerService: PhotoPickerServiceType {
+    func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data {
+        return Data()
+    }
+}
--- LMessenger/View/Profile/MyProfileViewModel.swift
+++ LMessenger/View/Profile/MyProfileViewModel.swift
@@ -5,12 +5,21 @@
 //
 
 import Foundation
+import SwiftUI
+import PhotosUI
 
 @MainActor // MainActor를 추가하면 해당되는 이 클래스 안에 있는 프로퍼티들이 메인 엑터에서 엑세스할 수 있움
 class MyProfileViewModel: ObservableObject {
     
     @Published var userInfo: User?
     @Published var isPresentedDescEditView: Bool = false
+    @Published var imageSelection: PhotosPickerItem? {
+        didSet {
+            Task {
+                await updateProfileImage(pickerItem: imageSelection)
+            }
+        }
+    }
     
     private let userId: String
     
@@ -31,6 +40,18 @@ class MyProfileViewModel: ObservableObject {
         do {
             try await container.services.userService.updateDescription(userId: userId, description: description)
             userInfo?.description = description
+        } catch {
+            
+        }
+    }
+    
+    func updateProfileImage(pickerItem: PhotosPickerItem?) async {
+        guard let pickerItem else { return }
+        
+        do {
+            let data = try await container.services.photoPickerService.loadTransferable(from: pickerItem)
+            // TODO: storage upload
+            // TODO: db update
         } catch {
             
         }
