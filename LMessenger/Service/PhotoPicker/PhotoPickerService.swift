76e311a139b7ce05f364e7ce26db976d1e5da49e
Sangmin Kim <offiksm0903@naver.com>
2025-02-18 17:23:18 +0900

    8-2: 채팅 기능 구현하기

--- LMessenger/Service/PhotoPicker/PhotoPickerService.swift
+++ LMessenger/Service/PhotoPicker/PhotoPickerService.swift
@@ -7,6 +7,7 @@
 import Foundation
 import SwiftUI
 import PhotosUI
+import Combine
 
 enum PhotoPickerError: Error {
     case importFailed
@@ -14,9 +15,30 @@ enum PhotoPickerError: Error {
 
 protocol PhotoPickerServiceType {
     func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data
+    func loadTransferable(from imageSelection: PhotosPickerItem) -> AnyPublisher<Data, ServiceError>
 }
 
 class PhotoPickerService: PhotoPickerServiceType {
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem) -> AnyPublisher<Data, ServiceError> {
+        Future { promise in
+            imageSelection.loadTransferable(type: PhotoImage.self) { result in
+                switch result {
+                case let .success(image):
+                    guard let data = image?.data else {
+                        promise(.failure(PhotoPickerError.importFailed))
+                            return
+                    }
+                    promise(.success(data))
+                case let .failure(error):
+                    promise(.failure(error))
+                }
+            }
+        }
+        .mapError { .error($0) }
+        .eraseToAnyPublisher()
+    }
+    
     func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data {
         
         guard let image = try await imageSelection.loadTransferable(type: PhotoImage.self) else {
@@ -29,5 +51,9 @@ class PhotoPickerService: PhotoPickerServiceType {
 class StubPhotoPickerService: PhotoPickerServiceType {
     func loadTransferable(from imageSelection: PhotosPickerItem) async throws -> Data {
         return Data()
+    }
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem) -> AnyPublisher<Data, ServiceError> {
+        Empty().eraseToAnyPublisher()
     }
 }
--- LMessenger/Service/UploadService.swift
+++ LMessenger/Service/UploadService.swift
@@ -5,8 +5,10 @@
 //
 
 import Foundation
+import Combine
 
 protocol UploadServiceType {
+    func uploadImage(source: UploadSourceType, data: Data) -> AnyPublisher<URL, ServiceError>
     func uploadImage(source: UploadSourceType, data: Data) async throws -> URL
 }
 
@@ -18,6 +20,12 @@ class UploadService: UploadServiceType {
         self.provider = provider
     }
     
+    func uploadImage(source: UploadSourceType, data: Data) -> AnyPublisher<URL, ServiceError> {
+        provider.upload(path: source.path, data: data, fileName: UUID().uuidString)
+            .mapError { .error($0) }
+            .eraseToAnyPublisher()
+    }
+    
     func uploadImage(source: UploadSourceType, data: Data) async throws -> URL {
         let url = try await provider.upload(path: source.path, data: data, fileName: UUID().uuidString)
         return url
@@ -25,6 +33,11 @@ class UploadService: UploadServiceType {
 }
 
 class StubUploadService: UploadServiceType {
+    
+    func uploadImage(source: UploadSourceType, data: Data) -> AnyPublisher<URL, ServiceError> {
+        Empty().eraseToAnyPublisher()
+    }
+    
     func uploadImage(source: UploadSourceType, data: Data) async throws -> URL {
         return URL(string: "")!
     }
